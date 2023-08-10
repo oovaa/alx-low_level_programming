0d15075ff1b4c7f48bfe3a2690c9da9f57cf0e12
oovaa <103980029+oovaa@users.noreply.github.com>
2023-08-10 10:37:33 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main() {
-  printf("with proper grammar, but the outcome is a piece of art");
+  printf("\"with proper grammar, but the outcome is a piece of art");
   return 0;
 }
