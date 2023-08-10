08e3fa1055e5243f98c5c0ef9becf39414694e72
oovaa <103980029+oovaa@users.noreply.github.com>
2023-08-10 10:46:13 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main() {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;
+
+/**
+ * main - program starts and ends in main
+ * main() takes no arguments
+ * Description: prints string to standard output using printf() funtion
+ * Return: main returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
