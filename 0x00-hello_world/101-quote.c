79f9c7fbdb78cab575e3332c1446dc993d55bfe7
oovaa <omarvx211@gmail.com>
2023-08-10 14:46:11 +0200

    finally at 2023-08-10

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints "and that piece of art is useful"
+ *  - Dora Korpar, 2015-10-19,
+ *              followed by a new line, to the standard error.
+ *
+ * Return: 1 (Error)
+ */
+int main(void)
+{
+
+return (1);
+}
