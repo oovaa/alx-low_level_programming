45bfebcafac2e4f4db915f36df3d961e25fe5e01
oovaa <omarvx211@gmail.com>
2023-08-11 15:39:42 +0200

    tranna solve 100-print_comb3.c  at 2023-08-11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Discription: a program that prints all possible different
+ *  combinations of two digits.
+ *
+ * Return: Alyways zero
+*/
+int main(void)
+{
+int i = 0;
+int a = i + 1;
+while (i < 9)
+{
+while (a < 9)
+{
+putchar(i + '0');
+putchar(a + '0');
+putchar(',');
+putchar(' ');
+a++;
+}
+a=i;
+i++;
+}
+putchar('\n');
+
+return (0);
+}
