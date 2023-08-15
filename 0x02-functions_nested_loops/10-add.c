2af59f5f46862b038b017860b59bc97da9ea916d
oovaa <omarvx211@gmail.com>
2023-08-15 15:56:06 +0200

    add at 15-15

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result.
+ *
+ *@a:  first perametot
+ *@b:  second perametot
+ * Return: Always 0 (Success)
+*/
+
+int add(int a, int b)
+{
+int sum = a + b;
+return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_last_digit - prints the 9 times table
+ * times_table - prints the 9 times table
  *
  * Return: Always 0 (Success)
 */
