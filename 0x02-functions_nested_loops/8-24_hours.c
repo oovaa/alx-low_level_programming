2bcad036dffb7cfe8cb8330c753c09621d82207b
oovaa <omarvx211@gmail.com>
2023-08-15 15:24:21 +0200

    fixd 8-24 bug at 15-15

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,38 +5,35 @@
  */
 void jack_bauer(void)
 {
-    int h1 = 0;
-    int h2 = 0;
-    int m1 = 0;
-    int m2 = 0;
-
-    while (1)
-    {
-        _putchar(h1 + '0');
-        _putchar(h2 + '0');
-        _putchar(':');
-        _putchar(m1 + '0');
-        _putchar(m2 + '0');
-        _putchar('\n');
-
-        m2++;
-        if (m2 == 10)
-        {
-            m2 = 0;
-            m1++;
-        }
-        if (m1 == 6)
-        {
-            m1 = 0;
-            h2++;
-        }
-        if (h2 == 4 && h1 < 2)
-        {
-            h2 = 0;
-            h1++;
-        }
-
-        if (h1 == 2 && h2 == 4 && m1 == 0 && m2 == 0)
-        break;   
-    }
+int h1 = 0;
+int h2 = 0;
+int m1 = 0;
+int m2 = 0;
+while (1)
+{
+_putchar(h1 + '0');
+_putchar(h2 + '0');
+_putchar(':');
+_putchar(m1 + '0');
+_putchar(m2 + '0');
+_putchar('\n');
+m2++;
+if (m2 == 10)
+{
+m2 = 0;
+m1++;
+}
+if (m1 == 6)
+{
+m1 = 0;
+h2++;
+}
+if (h2 == 9)
+{
+h2 = 0;
+h1++;
+}
+if (h1 == 2 && h2 == 4 && m1 == 0 && m2 == 0)
+break;
+}
 }
