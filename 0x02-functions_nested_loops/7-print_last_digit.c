2c8c43c2a75eec25f3e6ad47f76ce19996798516
oovaa <omarvx211@gmail.com>
2023-08-15 15:06:00 +0200

    7-print_last_digit.c  at 15-15

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - a function that prints the last digit of a number.
+ *
+ * @n: takes in integer type input for function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int print_last_digit(int n)
+{
+if (n < 0)
+n *= -1;
+
+n = n % 10;
+_putchar(n + '0');
+return (n);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+    int h1 = 0;
+    int h2 = 0;
+    int m1 = 0;
+    int m2 = 0;
+
+    while (1)
+    {
+        _putchar(h1 + '0');
+        _putchar(h2 + '0');
+        _putchar(':');
+        _putchar(m1 + '0');
+        _putchar(m2 + '0');
+        _putchar('\n');
+
+        m2++;
+        if (m2 == 10)
+        {
+            m2 = 0;
+            m1++;
+        }
+        if (m1 == 6)
+        {
+            m1 = 0;
+            h2++;
+        }
+        if (h2 == 4 && h1 < 2)
+        {
+            h2 = 0;
+            h1++;
+        }
+
+        if (h1 == 2 && h2 == 4 && m1 == 0 && m2 == 0)
+        break;   
+    }
+}
