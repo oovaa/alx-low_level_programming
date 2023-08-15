004c752489be6213df7060c90c15b36a095ebce7
oovaa <omarvx211@gmail.com>
2023-08-15 05:34:28 +0200

    starting 0x02-functions_nested_loops at 15-05

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription:  a program that prints _putchar,
+ * followed by a new line.
+ *
+ * Return: Alyways zero
+*/
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
