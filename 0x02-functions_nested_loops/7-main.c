73c8b6a5f90db7b0771e7a58effcdd1341daec1c
oovaa <omarvx211@gmail.com>
2023-08-15 15:19:21 +0200

    fixed print_last_digit bug at 15-15

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,18 +1,17 @@
+#include <limits.h>
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int r;
+	int r;
 
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
+	r = print_last_digit(INT_MIN);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
 }
