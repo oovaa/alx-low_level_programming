3b678c2bb9ce65f24cbd5d174bfee8ffdb4fcc5a
oovaa <omarvx211@gmail.com>
2023-08-16 13:37:03 +0200

    betty at 16-13

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * positive_or_negative - Entry point
  *
  * Description: print value of n satus; zero, positive or negative
  *
+ * @i: input num
+ * 
  * Return: Always 0 (Success)
 */
 
 void positive_or_negative(int i)
 {
-	if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
+if (i == 0)
+{
+	printf("%d is zero\n", i);
+}
+else if (i > 0)
+{
+	printf("%d is positive\n", i);
+}
+else
+{
+	printf("%d is negative\n", i);
+}
 }
