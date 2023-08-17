1896b74829db77f5d2a1fd023d03ba7d91cdaaf2
oovaa <omarvx211@gmail.com>
2023-08-17 18:53:46 +0200

    at 17-18

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,28 +12,24 @@
 
 int main(void)
 {
-int i;
-for (i = 1; i < 101; i++)
-{
-if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz ");
-}
-else if (i % 3 == 0)
-{
-printf("Fizz ");
+	int i;
 
-}
-else if (i % 5 == 0)
-{
-printf("Buzz ");
-}
-else
-{
-printf("%d ", i);
-}
+	for (i = 1; i <= 100; ++i)
+	{
+		if (i % 3 == 0 && !(i % 5 == 0))
+			printf("Fizz");
+		else if (i % 5 == 0 && !(i % 3 == 0))
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
 
-}
+		if (i != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
 
-return (0);
+	return (0);
 }
