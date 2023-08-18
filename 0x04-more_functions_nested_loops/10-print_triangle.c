9ec8acf62fa9557a72551dbe9a5c1f735f0b45bd
oovaa <omarvx211@gmail.com>
2023-08-18 05:04:17 +0200

    at 18-05

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,22 +10,21 @@
 
 void print_triangle(int size)
 {
-	int hght, base;
-
-	if (size <= 0)
-		_putchar('\n');
-	else
-	{
-		for (hght = 1; hght <= size; ++hght)
-		{
-			for (base = 1; base <= size; ++base)
-			{
-				if ((hght + base) <= size)
-					_putchar(' ');
-				else
-					_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+int h, b;
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (h = 1; h <= size; ++h)
+{
+for (b = 1; b <= size; ++b)
+{
+if ((h + b) <= size)
+	_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 }
