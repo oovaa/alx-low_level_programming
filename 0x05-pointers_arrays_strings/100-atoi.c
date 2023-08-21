e9847841ba301e13e1266023ed7f58a39c857f86
oovaa <omarvx211@gmail.com>
2023-08-21 13:03:50 +0200

    at 21-13

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,14 +9,13 @@ int _atoi(char *s)
 {
 int sign = 1;
 unsigned int num = 0;
-do
-{
+do {
 if (s[0] == '-')
 	sign *= -1;
 else if (s[0] >= '0' && s[0] <= '9')
 	num = (num * 10) + (s[0] - '0');
 else if (num > 0)
 	break;
-}while (*s++);
-return (num *sign);
+} while (*s++);
+return (num * sign);
 }
