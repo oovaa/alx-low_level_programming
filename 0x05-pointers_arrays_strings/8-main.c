0bb5192b405a51c7f96bdcad8fbe98c2d4d7380b
oovaa <omarvx211@gmail.com>
2023-08-21 09:36:24 +0200

    fix 5.8 at 21-09

--- 0x05-pointers_arrays_strings/8-main.c
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -1,19 +1,19 @@
-#include "main.h"
+#include <stdio.h>
+
+void print_array(int *, int);
 
 /**
- * main - check the code for
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int array[5];
+	int array[1] = {
+		98
+	};
 
-    array[0] = 98;
-    array[1] = 402;
-    array[2] = -198;
-    array[3] = 298;
-    array[4] = -1024;
-    print_array(array, 5);
-    return (0);
+	print_array(array, 0);
+	print_array(array, -1);
+	return (0);
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,12 +9,14 @@
  */
 void print_array(int *a, int n)
 {
-int i;
-for (i = 0; i < n -1; i++)
-{
-printf("%d, ", *(a + i));
-}
-printf("%d", *(a + i));
-putchar('\n');
+	int i;
 
+	for (i = 0; i < n; ++i)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
 }
