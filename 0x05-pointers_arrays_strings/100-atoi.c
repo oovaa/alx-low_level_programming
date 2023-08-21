435f09b9060b9182fa025f1dcf136dd161b20d01
oovaa <omarvx211@gmail.com>
2023-08-21 10:05:43 +0200

    tranna do advanced tasks at 21-10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,9 +3,20 @@
 /**
  * _atoi - function that convert a string to an integer.
  *@s: targeted string
- * Return: Always 0.
+ * Return: the converted integer value
  */
 int _atoi(char *s)
 {
-
+int sign = 1;
+int i =0;
+int num = 0;
+while (*(s + i) != '\0')
+{
+if (*s == '-')
+sign *= -1;
+if(s[i] >= '0' && s[i] <= '9')
+num += (num * 10) + (*s - 48);
+i++;
+}
+return (i * sign);
 }
