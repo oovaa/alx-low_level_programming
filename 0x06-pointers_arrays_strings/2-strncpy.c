34b461c3de40a3755fab703061584d01f524b891
oovaa <omarvx211@gmail.com>
2023-08-24 09:47:34 +0200

    fix 6.2 at 24-09

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
-for (i = 0; i < n; i++)
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
 dest[i] = src[i];
 }
+dest[i] = '\0';
 return (dest);
 }
