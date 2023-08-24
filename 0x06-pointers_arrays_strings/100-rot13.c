ffc6e7fc0021dafde35ba1976fd6696741b89ba0
oovaa <omarvx211@gmail.com>
2023-08-24 11:42:06 +0200

    tasks till 6.101 at 24-11

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - a function that encodes a string using rot13.
+ * @s: targeted string
+ * Return: the string coded
+ */
+
+char *rot13(char *s)
+{
+char src[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char des[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+/* if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
+ */{
+for (j = 0; src[j] != '\0'; j++)
+{
+if (s[i] == src[j])
+{
+s[i] = des[j];
+break;
+}
+}
+}
+}
+return (s);
+}
---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_number - unction that prints an integer.
+ * @n: the number
+ *
+*/
+
+
+void print_number(int n)
+{
+int h = 0;
+if (n < 0)
+{
+_putchar('-');
+n *= -1;
+}
+if (n == 0)
+_putchar('0');
+
+
+while (n != 0)
+{
+h = 10 * h + n %10;
+n /= 10; 
+}
+while (h != 0)
+{
+_putchar((h % 10) + '0');
+h /= 10; 
+}
+}
