d7503a9f077e5379f2ba98e95c08bcdca0d9ca27
oovaa <omarvx211@gmail.com>
2023-08-24 12:25:14 +0200

    6.6.3 at 24-12

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,34 @@
 #include "main.h"
+
 /**
- * cap_string - capitalizes most of the words in a string.
- * @s: analized string.
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
  *
- * Return: String with all words capitalized.
+ * Return: the resulting string
  */
 char *cap_string(char *s)
 {
-int i, j;
-int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-i = 0;
-while (s[i] != '\0')
-{
-if (s[i] >= 'a' && s[i] <= 'z')
-{
-for (j = 0; j <= 12; j++)
-{
-if (a[j] == s[i - 1])
-{
-s[i] = s[i] - 32;
-}
-}
-}
-i++;
-}
-return (s);
+	int i, j;
+
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
 }
