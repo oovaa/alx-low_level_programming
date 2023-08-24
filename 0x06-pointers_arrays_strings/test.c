9541b5a712f13ba69e62b52eca2c807ba524ef09
oovaa <omarvx211@gmail.com>
2023-08-24 16:39:32 +0200

    103 at 24-16

--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[]) {
+long long convertToLongLong(const char *str) {
+    long long result = 0;
+    int i = 0;
 
-    printf("%d\n",'s' - 'S');
+    while (str[i] != '\0') {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+
+    return result;
+}
+
+int main() {
+    const char *n = "123456789243457436782357457567847768578564568587687677458673473456"
+                    "3456453743756756784458";
+
+    long long a = convertToLongLong(n);
+
+    printf("%lld\n", a);
+
     return 0;
 }
