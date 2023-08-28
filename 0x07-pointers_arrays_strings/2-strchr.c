cfb3940fdcc20aae25afe2a29a6557e4d8c4bb60
oovaa <omarvx211@gmail.com>
2023-08-28 12:15:04 +0200

    fix 7.2 at 28-12

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,15 @@
 
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
-{
-if (*s == c)
-return (s);
-s++;
-}
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
 
-
-return (NULL);
+	return (NULL);
 }
