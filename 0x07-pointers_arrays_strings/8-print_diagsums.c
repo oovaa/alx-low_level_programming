de0df7e1288b0c39c7dc85743ec8b4c45a065ba3
oovaa <omarvx211@gmail.com>
2023-08-28 12:33:09 +0200

    add betty things to task 7 at 28-12

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_diagsums - prints the sums of the two diagonals of a square matrix
+ * @a: pointer to start of matrix
+ * @size: width of matrix column
+ *
+ * Return: void
+ */
 
 
 void print_diagsums(int *a, int size)
