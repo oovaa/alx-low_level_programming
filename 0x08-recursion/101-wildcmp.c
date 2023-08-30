951484ab43fe8a4b331270bd73ea5a9e58f18b6e
oovaa <omarvx211@gmail.com>
2023-08-30 19:29:14 +0200

    last task at 30-19

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,32 +1,33 @@
 #include "main.h"
 
 /**
- * wildcmp -  function that compares two strings and returns
- * 1 if the strings can be considered identical, otherwise return 0.
- * @s1: the first string
- * @s2: the second string
- * Return: 1 if the strings can be considered identical, otherwise return 0.
-*/
-
+ * str_checker - check if two strings are identical.
+ * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @i: left index.
+ * @j: special index. (joker)
+ * Return: 1 if s is palindrome, 0 otherwise.
+ */
+int str_checker(char *s1, char *s2, int i, int j)
+{
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
+}
+/**
+ * wildcmp - check if strings could be considered identical
+ * @s1: base address for string.
+ * @s2: base address for string.
+ *
+ * Return: 1 if are considered identical.
+ */
 int wildcmp(char *s1, char *s2)
 {
-if (*s1 == '\0' && *s2 == '\0')
-return (1);
-
-if (*s1 == *s2 || *s2 == '?')
-return (wildcmp(s1 + 1, s2 + 1));
-
-if (*s2 == '*')
-{
-while (*(s2 + 1) == '*')
-s2++;
-
-if (wildcmp(s1, s2 + 1))
-return (1);
-
-if (*s1 != '\0' && wildcmp(s1 + 1, s2))
-return (1);
-}
-
-return (0);
+	return (str_checker(s1, s2, 0, 0));
 }
