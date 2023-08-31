b1f2747cb0326807f02bbb76235ac546a26b9d37
oovaa <omarvx211@gmail.com>
2023-08-31 04:27:27 +0200

    is_palindrome at 31-04

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,39 @@
-#include "main.h" 
-  
- int check_pal(char *s, int i, int len); 
- int _strlen_recursion(char *s); 
-  
- /** 
-  * is_palindrome - checks if a string is a palindrome 
-  * @s: string to reverse 
-  * 
-  * Return: 1 if it is, 0 it's not 
-  */ 
- int is_palindrome(char *s) 
- { 
-if (*s == 0) 
-return (1); 
-return (check_pal(s, 0, _strlen_recursion(s))); 
- } 
-  
- /** 
-  * _strlen_recursion - returns the length of a string 
-  * @s: string to calculate the length of 
-  * 
-  * Return: length of the string 
-  */ 
- int _strlen_recursion(char *s) 
- { 
-if (*s == '\0') 
-return (0); 
-return (1 + _strlen_recursion(s + 1)); 
- } 
-  
- /** 
-  * check_pal - checks the characters recursively for palindrome 
-  * @s: string to check 
-  * @i: iterator 
-  * @len: length of the string 
-  * 
-  * Return: 1 if palindrome, 0 if not 
-  */ 
- int check_pal(char *s, int i, int len) 
- { 
-if (*(s + i) != *(s + len - 1)) 
-return (0); 
-if (i >= len) 
-return (1); 
-return (check_pal(s, i + 1, len - 1)); 
- }
+#include "main.h"
+/**
+ * _strlen_recursion - Prints the length of a string.
+ * @s: the string to be printed
+ * Return: the length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+/**
+ * pal_checker - check if s is palindrome.
+ * @s: string base address.
+ * @i: left index.
+ * @j: rigth index.
+ * Return: 1 if s is palindrome, 0 otherwise.
+ */
+int pal_checker(char *s, int i, int j)
+{
+	if (s[i] == s[j])
+		if (i > j / 2)
+			return (1);
+		else
+			return (pal_checker(s, i + 1, j - 1));
+	else
+		return (0);
+}
+/**
+ * is_palindrome - check if s is palindrome
+ * @s: base address for string.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	return (pal_checker(s, 0, _strlen_recursion(s) - 1));
+}
