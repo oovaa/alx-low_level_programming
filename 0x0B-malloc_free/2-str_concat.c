5e9508daf12fe97dbbca3ba2394e382cc5d21a6b
oovaa <omarvx211@gmail.com>
2023-09-04 13:57:07 +0200

    task B.2 at 04-13

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "string.h"
+/**
+ * str_concat - ada
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to the two strings combined
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+char *re;
+int len;
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+len = strlen(s1) + strlen(s2) + 1;
+re = (char *) malloc(len *sizeof(char) + 1);
+
+if (re == NULL)
+return (NULL);
+
+strcpy(re, s1);
+strcat(re, s2);
+
+return (re);
+}
+
