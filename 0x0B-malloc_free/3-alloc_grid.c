24bed1507ed93da4d90ac050fd4615ea40b3a837
oovaa <omarvx211@gmail.com>
2023-09-04 14:15:59 +0200

    fix B.3 at 04-14

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,20 +9,25 @@
 int **alloc_grid(int width, int height)
 {
 
-int **grid, i;
+int **grid, i, j;
 
 if (width == 0 || height == 0)
 return (NULL);
 
-grid = (int **)malloc(width * sizeof(int *));
+grid = (int **)malloc(height * sizeof(int *));
 if (grid == NULL)
 return (NULL);
 
 for (i = 0; i < height; i++)
 {
-grid[i] = (int *)calloc(sizeof(int), width);
+grid[i] = (int *)calloc(width, sizeof(int));
 if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+free(grid[i]);
+
 return (NULL);
+}
 }
 
 return (grid);
