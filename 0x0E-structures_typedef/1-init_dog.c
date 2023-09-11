8b4c83e604c658739a8b349ddd4790b8a37acc25
oovaa <omarvx211@gmail.com>
2023-09-11 11:47:59 +0200

    fix E.1 at 11-11

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,8 +12,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d->age = age;
-d->name = name;
-d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
 
