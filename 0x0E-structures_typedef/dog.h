0628675b0c2fbbda26e33fc2b8fe42e5177d8a9e
oovaa <omarvx211@gmail.com>
2023-09-11 11:50:19 +0200

    at 11-11

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG
 #define DOG
+
 /**
  * struct dog - dog class
  *
@@ -12,16 +13,18 @@
 
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
 
-#endif /*  !_DOG_H_ */
+#endif
