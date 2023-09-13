40ca6c7a5d732e3485ec23af224918baa4aacd19
oovaa <omarvx211@gmail.com>
2023-09-13 15:02:11 +0200

    F.100.3 at 13-15

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,6 +2,27 @@
 #include <stdio.h>
 
 /**
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ *
+ * Return: void
+ */
+void print_opcodes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+}
+
+/**
  * main - prints the opcodes of its own main function
  * @argc: number of arguments passed to the function
  * @argv: array of pointers to arguments
@@ -10,30 +31,19 @@
  */
 int main(int argc, char **argv)
 {
-int n, i;
-char *a;
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
+	int n;
 
-if (n < 0)
-{
-printf("Error\n");
-return (2);
-}
-
-n = atoi(argv[1]);
-a = (char *)&main;
-
-for (i = 0; i < n; i++)
-{
-printf("%.2hhx", a[i]);
-if (i < n - 1)
-printf(" ");
-}
-printf("\n");
-
-return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
 }
