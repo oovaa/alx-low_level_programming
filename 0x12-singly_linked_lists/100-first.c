f0b15718efcdaa1b77042b94c62886f30262a404
oovaa <omarvx211@gmail.com>
2023-09-21 16:16:22 +0200

    at 21-16

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+
+void before_main(void) __attribute__((constructor));
+
+/**
+ * before_main - a function that excutes before main
+ *
+*/
+
+void before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
