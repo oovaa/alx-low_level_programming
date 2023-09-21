40a8a539daae1fe0f1f7656f9ea25d5a439ca2a6
oovaa <omarvx211@gmail.com>
2023-09-21 16:04:32 +0200

    at 21-16

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,12 +11,17 @@
 void free_list(list_t *head)
 {
 list_t *cur;
+
+if (!head)
+return;
+
 cur = head;
 while (cur != NULL)
 {
+if (cur)
+free(cur);
 if (cur->str)
 free(cur->str);
-free(cur);
 
 cur = cur->next;
 }
