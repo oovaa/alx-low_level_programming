574cc826a6f2c06a0688d160c2826789508c5c62
oovaa <omarvx211@gmail.com>
2023-09-26 13:10:50 +0200

    at 26-13

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,28 +12,30 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *tem;
-unsigned int i;
+	unsigned int i;
+	listint_t *tmp, *next;
 
 
-if (index == 0)
-{
-*head = (*head)->next;
-return (1);
-}
-
-tem = *head;
-
-for (i = 0; i < index - 1; i++)
-{
-if (tem == NULL)
-return (-1);
-
-tem = tem->next;
-}
-tem->next = tem->next->next;
-
-return (1);
+	if (head == NULL || *head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+		return (1);
+	}
+	tmp = *head;
+	for (i = 0; i < index - 1; i++)
+	{
+		if (tmp->next == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+	next = tmp->next;
+	tmp->next = next->next;
+	free(next);
+	return (1);
 }
 
 
