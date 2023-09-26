83051b897ee7c34e9288383829c695a83b5d0828
oovaa <omarvx211@gmail.com>
2023-09-26 12:24:41 +0200

    at 26-12

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 /**
- * pop_listcint - unction that deletes the head node of a listint_t linked list
+ * pop_listint - unction that deletes the head node of a listint_t linked list
  * @head: the head of the linked list
  * Return: if the linked list is empty return 0 or
  *  returns the head node’s data (n).
  *
 */
 
-int pop_listcint(listint_t **head)
+int pop_listint(listint_t **head)
 {
 	listint_t *start;
 	int n;
