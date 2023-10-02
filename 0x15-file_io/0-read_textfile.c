fe38f73ce56de9d001fbb9008f4b3db6cd4d1e55
oovaa <omarvx211@gmail.com>
2023-10-02 13:58:36 +0200

    at 02-13

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,32 +12,31 @@
  *           function should read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
- * O/w - the actual number of bytes the function can read and print.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t r, w, o;
+	ssize_t o, r, w;
+	char *buffer;
 
-char *buf;
+	if (filename == NULL)
+		return (0);
 
-if (filename == NULL)
-return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-buf = malloc(sizeof(char) * letters);
-if (buf == NULL)
-return (0);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
 
-o = open(filename, O_RDONLY);
-r = read(o, buf, letters);
-w = write(STDOUT_FILENO, buf, r);
+	free(buffer);
+	close(o);
 
-if (o == -1 || r == -1 || w == -1 || w != r)
-{
-free(buf);
-return (0);
-}
-
-free(buf);
-return (w);
-
+	return (w);
 }
