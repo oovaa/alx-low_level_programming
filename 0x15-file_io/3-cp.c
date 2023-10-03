864856e9822a2d2129684d99fb370e1dca6068f8
oovaa <omarvx211@gmail.com>
2023-10-03 04:55:11 +0200

    at 03-04

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -98,36 +98,24 @@ check97(argc);
 fd_from = open(argv[1], O_RDONLY);
 
 check98((ssize_t)fd_from, argv[1], -1, -1);
-
 file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-
 fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
-
 check99((ssize_t)fd_to, argv[2], fd_from, -1);
-
 lenr = 1024;
 
 while (lenr == 1024)
 {
 	lenr = read(fd_from, buffer, 1024);
-
 	check98(lenr, argv[1], fd_from, fd_to);
-
 	lenw = write(fd_to, buffer, lenr);
 
 	if (lenw != lenr)
-
 		lenw = -1;
-
 	check99(lenw, argv[2], fd_from, fd_to);
 }
-
 close_to = close(fd_to);
-
 close_from = close(fd_from);
-
 check100(close_to, fd_to);
-
 check100(close_from, fd_from);
 return (0);
 }
