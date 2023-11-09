129addc3a30b56596ad3b6fe8b771d5907e794ba
oovaa <omarvx211@gmail.com>
2023-11-09 21:52:33 +0200

    last at 09-21

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,36 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define KEY_LENGTH 64
+#include <time.h>
 
 /**
- * generate_key - Generates a key based on the given username.
+ * generate_key - Generates a key based on the provided username.
  * @username: The input username.
- * @key: The buffer to store the generated key.
+ * @key: The generated key (output).
  */
+
 void generate_key(char *username, char *key)
 {
-	snprintf(key, KEY_LENGTH,
-				"%s%s%s%s", username, "S3cr3tK3y", "OpenAI", "Challenge");
+	size_t len = strlen(username);
+	size_t i, b;
+	int add = 0;
+	char l[] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+
+	key[0] = l[(len ^ 59) & 63];
+
+	for (i = 0; i < len; i++)
+		add += username[i];
+	key[1] = l[(add ^ 79) & 63];
+
+	b = 1;
+	for (i = 0; i < len; i++)
+		b *= username[i];
+	key[2] = l[(b ^ 85) & 63];
+
+	b = username[0];
+	for (i = 0; i < len; i++)
+		if ((char)b <= username[i])
+			b = username[i];
+	srand(b ^ 14);
+	key[3] = l[rand() & 63];
+
+	b = 0;
+	for (i = 0; i < len; i++)
+		b += username[i] * username[i];
+	key[4] = l[(b ^ 239) & 63];
+
+	b = 0;
+	for (i = 0; (char)i < username[0]; i++)
+		b = rand();
+	key[5] = l[(b ^ 229) & 63];
+
+	key[64 - 1] = '\0';
 }
-/**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings representing the command-line arguments.
- *
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure.
- */
+
 int main(int argc, char *argv[])
 {
-	char key[KEY_LENGTH];
+	char key[64];
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: %s <username>\n", argv[0]);
+		fprintf(stderr, "Correct usage: %s <username>\n", argv[0]);
 		return (EXIT_FAILURE);
 	}
-
 	generate_key(argv[1], key);
 
 	printf("%s\n", key);
