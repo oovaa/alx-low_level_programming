6152f64fd55a78d56e3d403421be7d03fb657292
oovaa <omarvx211@gmail.com>
2023-11-13 22:33:49 +0200

    fix 100py at 13-22

--- 0x18-dynamic_libraries/cdl.c
+++ 0x18-dynamic_libraries/cdl.c
@@ -4,7 +4,7 @@ int add(int a, int b) { return a + b; }
 int sub(int a, int b) { return a - b; }
 int mul(int a, int b) { return a * b; }
 int div(int a, int b) { return a / b; }
-int mod(int a, int b) { return a + b; }
+int mod(int a, int b) { return a % b; }
 
 int main()
 {
