ed41b1dd1fff1093ced42c47aa1329cbf960d464
oovaa <omarvx211@gmail.com>
2023-11-13 22:46:36 +0200

    at 13-22

--- 0x18-dynamic_libraries/cdl.c
+++ 0x18-dynamic_libraries/cdl.c
@@ -6,8 +6,8 @@ int mul(int a, int b) { return a * b; }
 int div(int a, int b) { return a / b; }
 int mod(int a, int b) { return a % b; }
 
-int main()
+int main(void)
 {
-	
-	return 0;
+
+	return (0);
 }
