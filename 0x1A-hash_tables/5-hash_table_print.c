0c1b6a388dcb2c4a56485b7d8b0edd7a4f38637e
oovaa <omarvx211@gmail.com>
2023-11-30 15:24:08 +0200

    task 5 at 30-15

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+
+/**
+ * print_list - prints the elements of a linked list
+ * @node: pointer to the head of the linked list
+ *
+ * Description:
+ *   This helper function prints the key-value pairs of a linked list.
+ *   It recursively traverses the list and prints each node's key and value.
+ */
+void print_list(hash_node_t *node)
+{
+	if (!node)
+		return;
+
+	if (node->next)
+	{
+		printf("'%s': '%s', ", node->key, node->value);
+		print_list(node->next);
+	}
+	else
+		printf("'%s': '%s'", node->key, node->value);
+
+
+}
+
+/**
+ * hash_table_print - prints the elements of a hash table
+ * @ht: pointer to the hash table
+ *
+ * Description:
+ *   This function prints the key-value pairs of the elements in the hash
+ *   table. It traverses each index in the hash table array and prints the
+ *   key-value pairs of the linked list at that index.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i = 0;
+
+	if (!ht || !ht->array)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		print_list(ht->array[i]);
+	}
+	printf("}\n");
+}
