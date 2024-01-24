a9c1c4235d5b03efe6141935d519279c55b9fe9a
oovaa <omarvx211@gmail.com>
2024-01-24 14:14:28 +0200

    started task 5 at 24-14

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,9 @@
+"""
+task 5
+Create a function def island_perimeter(grid): that returns the
+perimeter of the island described in grid:
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
