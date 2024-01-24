ea7236eea378a8a5551ca8b2bc8fd873a0ed2299
oovaa <omarvx211@gmail.com>
2024-01-24 15:31:01 +0200

    redo task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,16 +12,16 @@ def island_perimeter(grid):
     for r in range(len(grid)):
         for c in range(len(grid[0])):
             if grid[r][c] == 1:
-                # Check transitions in the row
-                if c == 0 or grid[r][c - 1] == 0:
-                    perimeter += 1  # Transition from water to land
-                if c == len(grid[0]) - 1 or grid[r][c + 1] == 0:
-                    perimeter += 1  # Transition from land to water
 
-                # Check transitions in the column
-                if r == 0 or grid[r - 1][c] == 0:
-                    perimeter += 1  # Transition from water to land
-                if r == len(grid) - 1 or grid[r + 1][c] == 0:
-                    perimeter += 1  # Transition from land to water
+                if c == 0 or grid[r][c-1] == 0:
+                    perimeter += 1
+
+                if c == (len(grid)-1) or grid[r][c+1] == 0:
+                    perimeter += 1
+
+                if r == 0 or grid[r-1][c] == 0:
+                    perimeter += 1
+                if r == (len(grid[0])-1) or grid[r+1][c] == 0:
+                    perimeter += 1
 
     return perimeter
