dcc7519290ac89d3a29c35fc760a0e3a1382d322
oovaa <omarvx211@gmail.com>
2024-01-24 15:47:59 +0200

    hotfix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,12 +16,12 @@ def island_perimeter(grid):
                 if c == 0 or grid[r][c-1] == 0:
                     perimeter += 1
 
-                if c == (len(grid)-1) or grid[r][c+1] == 0:
+                if c == (len(grid[0])-1) or grid[r][c+1] == 0:
                     perimeter += 1
 
                 if r == 0 or grid[r-1][c] == 0:
                     perimeter += 1
-                if r == (len(grid[0])-1) or grid[r+1][c] == 0:
+                if r == (len(grid)-1) or grid[r+1][c] == 0:
                     perimeter += 1
 
     return perimeter
