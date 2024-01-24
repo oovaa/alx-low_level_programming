750b16244cbd8bf94cf21029c2985393f66a92e9
oovaa <omarvx211@gmail.com>
2024-01-24 09:35:17 +0200

    started make files at 24-09

--- 0x1A-hash_tables/test.c
+++ 0x1A-hash_tables/test.c
@@ -43,60 +43,166 @@ shash_node_t *create_node(const char *key, const char *value)
 
 }
 
-int add_to_sorted_list(shash_table_t *sht, const char *key, const char *value)
-	{
-		shash_node_t *node, *tmp;
+int add_to_sorted_list(shash_table_t *sht, shash_node_t *node)
+{
+    shash_node_t *tmp;
 
-		node = create_node(key, value);
-		if (!node)
-			return 0;
+    if (!sht || !node)
+        return 0;
 
-		if (!sht->shead)
-		{
-			sht->shead = sht->stail = node;
-			return 1;
-		}
-		tmp = sht->shead;
-		while (tmp)
-		{
-			if (strcmp(tmp->key, node->key) > 0)
-			{
-				node->snext = tmp;
-				node->sprev = tmp->sprev;
-				tmp->sprev = node;
-				if (node->sprev)
-					node->sprev->snext = node;
-				else
-					sht->shead = node;
-				return 1;
-			}
-			tmp = tmp->snext;
-		}
+    if (!sht->shead)
+    {
+        sht->shead = sht->stail = node;
+        return 1;
+    }
 
-		node->sprev = sht->stail;
-		sht->stail->snext = node;
-		sht->stail = node;
+    tmp = sht->shead;
+    while (tmp)
+    {
+        if (strcmp(tmp->key, node->key) > 0)
+        {
+            node->snext = tmp;
+            node->sprev = tmp->sprev;
 
-		return 1;
-	}
+            if (node->sprev)
+                node->sprev->snext = node;
+            else
+                sht->shead = node;
+
+            tmp->sprev = node;
+            return 1;
+        }
+        tmp = tmp->snext;
+    }
+
+    node->sprev = sht->stail;
+    if (sht->stail)
+        sht->stail->snext = node;
+    sht->stail = node;
+
+    return 1;
+}
+
 
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	{
-		unsigned int i, index;
+		unsigned int index;
 		shash_node_t *newnode, *tmp; 
+		shash_node_t **arr;
+
+    	if (!ht || !key || !value)
+     	   return 0;
 
 		newnode = create_node(key, value);
-	
+		arr = ht->array;
+		if (!newnode || ! arr)
+			return 0;
+		
+		index = key_index((const unsigned char *)key, ht->size);
+		tmp = arr[index];
+		while (tmp)
+		{
+			if (strcmp(tmp->key, key) == 0)
+			{
+				free(tmp->key);
+				tmp->key = strdup(key);
+				free(newnode->key);
+				free(newnode->value);
+				free(newnode);
+				return 1;
+			}
+			tmp = tmp->next;
+		}
+		newnode->next = arr[index];
+		arr[index] = newnode;
 
-
-		add_to_sorted_list(ht, key, value);
-		return 0;
+		add_to_sorted_list(ht, newnode);
+		return 1;
 	}
 
-char *shash_table_get(const shash_table_t *ht, const char *key);
 
-void shash_table_print(const shash_table_t *ht);
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	shash_node_t *tmp;
 
-void shash_table_print_rev(const shash_table_t *ht);
+    if (!ht || !key) {
+        return NULL;
+    }
+
+	tmp = ht->array[key_index((const unsigned char *)key, ht->size)];
+
+	while (tmp)
+	{
+		if (strcmp(key, tmp->key) == 0)
+			return tmp->value;
+		tmp = tmp->next;
+	}
+
+	return NULL;
+}
+
+void shash_table_print(const shash_table_t *ht)
+{
+	shash_node_t *tmp = ht->shead;
+
+	printf("{");
+	if (!tmp)
+	{
+		printf("it is empty");
+		return;
+	}
+	
+	while (tmp->snext)
+	{
+		printf("%s: %s, ",tmp->key, tmp->value);
+		tmp = tmp->snext;
+	}
+	printf("%s: %s}\n",tmp->key, tmp->value);
+}
+
+void shash_table_print_rev(const shash_table_t *ht)
+{
+
+	shash_node_t *tmp = ht->stail;
+
+	printf("{");
+	if (!tmp)
+	{
+		printf("it is empty");
+		return;
+	}
+	
+	while (tmp->sprev)
+	{
+		printf("%s: %s, ",tmp->key, tmp->value);
+		tmp = tmp->sprev;
+	}
+	printf("%s: %s}\n",tmp->key, tmp->value);
+	
+}
 
 void shash_table_delete(shash_table_t *ht);
+
+int main() {
+    shash_table_t *ht;
+
+    ht = shash_table_create(1024);
+    shash_table_set(ht, "y", "0");
+    shash_table_print_rev(ht);
+    shash_table_set(ht, "j", "1");
+    shash_table_print_rev(ht);
+    shash_table_set(ht, "c", "2");
+    shash_table_print_rev(ht);
+    shash_table_set(ht, "b", "3");
+    shash_table_print_rev(ht);
+    shash_table_set(ht, "z", "4");
+    shash_table_print_rev(ht);
+    shash_table_set(ht, "n", "5");
+    shash_table_print_rev(ht);
+    shash_table_set(ht, "a", "6");
+    shash_table_print_rev(ht);
+    shash_table_set(ht, "m", "7");
+    shash_table_print_rev(ht);
+    shash_table_print(ht);
+    return (EXIT_SUCCESS);
+}
