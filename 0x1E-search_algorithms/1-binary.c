c003676f8a102d52f709e584897a87a8ad520f38
oovaa <omarvx211@gmail.com>
2024-03-12 10:10:36 +0200

    fix task 1 betty at 12-10

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,13 +1,12 @@
 #include "search_algos.h"
 
 /**
-
-parr - a function that prints an array of integers
-@s: the starting index of the array to print from
-@e: the ending index of the array to print to
-@arr: a pointer to the first element of the array to print
-This function prints the elements of the array from index 's' to 'e'
-**/
+ * parr - a function that prints an array of integers
+ * @s: the starting index of the array to print from
+ * @e: the ending index of the array to print to
+ * @arr: a pointer to the first element of the array to print
+ * This function prints the elements of the array from index 's' to 'e'
+ **/
 
 void parr(int s, int e, int *arr)
 {
